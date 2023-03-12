2aa0938e5a4388b1b72aa2d802ac34a40d61e70d
Wai Lon <rxdvd0@gmail.com>
2023-03-12 09:43:26 +0000

    implement PiFromPolygons class

--- classes/pi.py
+++ classes/pi.py
@@ -1,17 +1,12 @@
+import math
 from polygon import RegularPolygon
 
 class PiFromPolygons:
-    def __init__(self):
-        pass
-    
-    @property
-    def interior_polygon(self):
-        return 'RegularPolygon object for interior'
-
-    @property
-    def exterior_polygon(self):
-        return 'RegularPolygon object for exterior'
+    def __init__(self, num_sides):
+        self._num_sides = num_sides
+        self._interior_polygon = RegularPolygon(num_sides, 1, 0)
+        self._exterior_polygon = RegularPolygon(num_sides, 1, math.tan(math.pi / num_sides))
     
     @property
     def pi(self):
-        return 'pi approximation'
+        return (self._interior_polygon.area + self._exterior_polygon) / 2
