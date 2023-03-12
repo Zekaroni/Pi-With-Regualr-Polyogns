3f177658c409a5df96cbf1fcc5e17adb7493f29b
Wai Lon <rxdvd0@gmail.com>
2023-03-12 09:43:08 +0000

    implement RegularPolygon class

--- classes/polygon.py
+++ classes/polygon.py
@@ -1,13 +1,31 @@
+import math
+
 class RegularPolygon:
-    def __init__(self):
+    def __init__(self, num_sides, x, y):
+        self._num_sides = num_sides
+        self._vertices = [(x, y)]
         pass
 
     @property
     def area(self):
-        # calculate area
-        return 'area'
+        x, y = self._vertices[0]
+        hyp = math.sqrt(x ** 2 + y ** 2)
+        angle = math.pi / self._num_sides
+        area = self._num_sides * (hyp ** 2) * math.sin(angle) * math.cos(angle)
+        return area
 
     @property
     def vertices(self):
-        # calculate coordinates for each vertex
-        return 'list with vertices'
+        if(len(self._vertices) == self._num_sides):
+            return self._vertices
+        angle_of_rotation = 360 / self._num_sides
+        for i in range(1, self._num_sides):
+            self._vertices.append(self.rotate(*self._vertices[0], angle_of_rotation * i))
+        return self._vertices
+        
+    @staticmethod
+    def rotate(x, y, angle):
+        angle = math.radians(angle)
+        rotated_x = math.cos(angle) * x - math.sin(angle) * y
+        rotated_y = math.sin(angle) * x + math.cos(angle) * y
+        return rotated_x, rotated_y
