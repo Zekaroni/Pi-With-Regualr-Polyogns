e5da1a8d0371214d8240ad35dae7c1e0a1db2fe1
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-03-15 21:47:13 -0400

    Updated files + Transfer to new repo

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-import pygame as PYGAME
-import pygame_widgets
+# BUG: When launching on laptop (Ubuntu), the screen starts not rendered until slider update 
+# BUG: Crashes on Raspberian
+
+# Import error handling
+from os import system
+__found_pygame = False
+__found_pygame_widgets = False
+try:
+    import pygame as PYGAME
+    __found_pygame = True
+except ModuleNotFoundError: pass
+try:
+    import pygame_widgets
+    __found_pygame_widgets = True
+except ModuleNotFoundError: pass
+if not __found_pygame or not __found_pygame_widgets:
+    if input("Packages are missing. Would you like to install these automatically?  (y/n): ").lower() != 'y':
+        exit()
+if not __found_pygame: system('pip install pygame'); import pygame as PYGAME
+if not __found_pygame_widgets: system('pip install pygame_widgets'); import pygame_widgets
+del __found_pygame, __found_pygame_widgets, system
+
 from pygame_widgets.slider import Slider
 from pygame_widgets.textbox import TextBox
 
 from classes.pi import PiFromPolygons as CreatePolygon
-
-# BUG: When launching on laptop (Ubuntu), the screen starts not rendered until slider update
-# BUG: Crashes on Raspberian
 
 class Window():
     def __init__(self):
         self.NUM_OF_SHAPES = 2121 # 2121 needed for 6 point precision
         self.POLYGONS = [CreatePolygon(i) for i in range(3, self.NUM_OF_SHAPES+1)] # List of all polygons and verticies
         self.PRIMARY_COLOUR = ((159, 175, 189)) # (r,b,g)
-        self.INNER_COLOUR = (89,197,219) # TODO
+        self.INNER_COLOUR = (89,197,219)
         self.OUTER_COLOUR = (74,104,188)
         self.CIRCLE_COLOUR = (0,0,0)
         self.LINE_WIDTH = 3
@@ -29,10 +46,10 @@ class Window():
     def DrawUI(self):
         self.slider = Slider(
                     self.WINDOW,
-                    self.SCREEN_SIZE[1]/20, # x
-                    self.SCREEN_SIZE[1]-round(self.SCREEN_SIZE[0]/20), # y
-                    self.SCREEN_SIZE[0]-round(self.SCREEN_SIZE[0]/17.5), # width
-                    round(self.SCREEN_SIZE[0]/30), # height
+                    round(self.SCREEN_SIZE[0]/40), # x
+                    round(self.SCREEN_SIZE[1]-self.SCREEN_SIZE[1]/20), # y
+                    round(self.SCREEN_SIZE[0]-self.SCREEN_SIZE[0]/20), # width
+                    round(self.SCREEN_SIZE[1]/30), # height
                     min=0,
                     max=self.NUM_OF_SHAPES-3,
                     step=1,
@@ -40,18 +57,18 @@ class Window():
                 )
         self.label_names_box = TextBox(
                     self.WINDOW,
-                    round(self.SCREEN_SIZE[0]/2)-round((self.SCREEN_SIZE[0]/7.75)/2),
+                    round(self.CENTER[0]-self.CENTER[0]/7),
                     0,
-                    self.SCREEN_SIZE[0]/7.75,
-                    self.SCREEN_SIZE[0]/50,
-                    fontSize=round(self.SCREEN_SIZE[0]/64)
+                    round(self.SCREEN_SIZE[0]/7),
+                    round(self.SCREEN_SIZE[0]/50),
+                    fontSize=round(self.SCREEN_SIZE[0]/60)
                 )
         self.inbtween_values_box = TextBox(
                     self.WINDOW,
-                    round(self.SCREEN_SIZE[0]/2)-round((self.SCREEN_SIZE[0]/7.75)/2),
+                    round(self.CENTER[0]-self.CENTER[0]/7.75),
                     round(self.SCREEN_SIZE[1]/30),
-                    self.SCREEN_SIZE[0]/7.75,
-                    self.SCREEN_SIZE[0]/50,
+                    round(self.SCREEN_SIZE[0]/7.75),
+                    round(self.SCREEN_SIZE[0]/50),
                     fontSize=round(self.SCREEN_SIZE[0]/64)
                 )
         self.pi_average_box = TextBox(
@@ -64,19 +81,27 @@ class Window():
                 )
         self.side_amount_output_box = TextBox(
                     self.WINDOW,
-                    round(self.SCREEN_SIZE[0]/2)-25,
-                    round(self.SCREEN_SIZE[1])-round(self.SCREEN_SIZE[1]/7.5),
+                    self.CENTER[0]-self.CENTER[1]/30,
+                    self.SCREEN_SIZE[1]-self.SCREEN_SIZE[1]/7.5,
                     self.SCREEN_SIZE[0]/30,
                     self.SCREEN_SIZE[0]/50,
                     fontSize=round(self.SCREEN_SIZE[0]/64)
                 )
         self.radius_box = TextBox(
                     self.WINDOW,
-                    self.CENTER[0],
-                    self.CENTER[1]+self.CENTER[0]/100,
+                    self.CENTER[0]+self.CENTER[0]/20,
+                    self.CENTER[1]+self.CENTER[1]/100,
                     self.SCREEN_SIZE[0]/30,
-                    self.SCREEN_SIZE[0]/50,
-                    fontSize=round(self.SCREEN_SIZE[0]/64)
+                    self.SCREEN_SIZE[0]/70,
+                    fontSize=round(self.SCREEN_SIZE[0]/76)
+                )
+        self.theta_box = TextBox(
+                    self.WINDOW,
+                    self.CENTER[0]-self.CENTER[0]/10,
+                    self.CENTER[1]-self.CENTER[1]/50,
+                    self.SCREEN_SIZE[0]/23,
+                    self.SCREEN_SIZE[0]/70,
+                    fontSize=round(self.SCREEN_SIZE[0]/76)
                 )
         
         self.label_names_box.disable() # Act as label instead of textbox
@@ -84,6 +109,7 @@ class Window():
         self.pi_average_box.disable()
         self.side_amount_output_box.disable() 
         self.radius_box.disable()
+        self.theta_box.disable()
         
         self.DrawShapes()
 
@@ -113,7 +139,7 @@ class Window():
         PYGAME.draw.circle( # Center dot
                         self.WINDOW,
                         self.CIRCLE_COLOUR,
-                        (self.SCREEN_SIZE[0]/2,self.SCREEN_SIZE[1]/2),
+                        self.CENTER,
                         self.SCALE,
                         1 
                     )
@@ -124,7 +150,13 @@ class Window():
                         (self.CENTER[0]+self.SCALE,self.CENTER[1]),
                         self.LINE_WIDTH
                     )
-        # TODO: Draw a line from the center to create the triangle
+        PYGAME.draw.line( # Line to form inner triangle
+                        self.WINDOW,
+                        self.INNER_COLOUR,
+                        self.CENTER,
+                        ((self.adjusted_vertices[0][-1][0]+self.adjusted_vertices[0][0][0])/2,(self.adjusted_vertices[0][-1][1]+self.adjusted_vertices[0][0][1])/2),
+                        self.LINE_WIDTH
+                    )
         PYGAME.draw.polygon( # Inner polygon
                         self.WINDOW, # Surface
                         self.INNER_COLOUR, # Colour
@@ -138,11 +170,12 @@ class Window():
                         self.LINE_WIDTH
                     )
 
-        self.label_names_box.setText(f"Inner Area    Outer Area")
+        self.label_names_box.setText(f"Inner Area      Outer Area")
         self.inbtween_values_box.setText(f"{format(round(self.active_polygons._interior_polygon.area, 6),'.6f')} < π < {format(round(self.active_polygons._exterior_polygon.area, 6), '.6f')}")
         self.pi_average_box.setText(f"Average: {format(self.active_polygons.pi,'.6f')}")
         self.side_amount_output_box.setText(f"{str(self.active_polygon_index + 3).rjust(4)}")
         self.radius_box.setText(f"r = 1")
+        self.theta_box.setText(f'θ = {round(180/(self.active_polygon_index+3),2)}')
         self._old_slider_value = self._slider_value
 
     def start(self):
@@ -152,7 +185,7 @@ class Window():
             for event in events:
                 if event.type == PYGAME.QUIT:
                     PYGAME.quit()
-                    quit()
+                    exit()
             
             self._slider_value = self.slider.getValue()
             if self._slider_value != self._old_slider_value:
